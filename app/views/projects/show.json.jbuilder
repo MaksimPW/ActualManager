3df861b31bfe90dcf73f38825a0849eb2d56a765
MaksimPW <ma.savichev@gmail.com>
2016-02-10 21:37:41 +0600

    Generate scaffold Projects which help SLIM template

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :description, :id_parent, :created_at, :updated_at
---
+++ spec/factories/projects.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :project do
+    name "MyString"
+    description "MyText"
+    id_parent 1
+  end
+end
