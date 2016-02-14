2b600e78a947f219d314d53ee08a1865583f9c4e
MaksimPW <ma.savichev@gmail.com>
2016-02-14 20:01:01 +0600

    Update views

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :project
+  validates :name, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :description, length: { maximum: 250 }
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :lists
+  validates :name, presence: true
+  validates :name, length: { maximum: 50 }
+  validates :description, length: { maximum: 250 }
 end
